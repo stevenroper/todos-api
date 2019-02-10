d846b761fdda56dcec6c7b94de692e6eeab89f88
Steven <steven.m.roper@gmail.com>
2019-02-09 18:29:33 -0700

    Create AuthorizeAPIRequest class.

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -4,12 +4,14 @@ module ExceptionHandler
   class AuthenticationError < StandardError; end
   class MissingToken < StandardError; end
   class InvalidToken < StandardError; end
+  class ExpiredSignature < StandardError; end
 
   included do
     rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
     rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
     rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::ExpiredSignature, with: :four_ninety_eight
 
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
@@ -20,6 +22,10 @@ module ExceptionHandler
 
   def four_twenty_two(e)
     json_response({ message: e.message }, :unprocessable_entity)
+  end
+
+  def four_ninety_eight(e)
+    json_response({ message: e.message }, :invalid_token)
   end
 
   def unauthorized_request(e)
--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,7 +1,7 @@
 class JsonWebToken
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
+  HMAC_SECRET = Rails.application.credentials.secret_key_base
 
-  def self.encode(payload, exp = 24.hours_from_now)
+  def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, HMAC_SECRET)
   end
