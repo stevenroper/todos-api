50cbf99fdee792e9433df33b34888447554fb245
Steven <steven.m.roper@gmail.com>
2019-02-20 20:57:49 -0700

    Create more robust db seeds for dev DB.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-50.times do
-  todo = Todo.create(title: Faker::Lorem.word, created_by: User.first.id)
-  todo.items.create(name: Faker::Lorem.word, done: false)
+5.times do |num|
+  User.create(
+    name: Faker::Name.name,
+    email: "test_user#{num + 1}@test.com",
+    password: 'abcd1234',
+    password_confirmation: 'abcd1234'
+  )
+end
+
+users = User.all
+
+users.each do |user|
+  10.times do
+    todo = Todo.create(title: Faker::Job.title, created_by: user.id)
+    5.times do
+      todo.items.create(name: Faker::Job.key_skill, done: false)
+    end
+  end
 end
